73b289ec20c1e81d731452e8ee850f5521b6b8f1
João Gabriel <joaogabriel-al@hotmail.com>
2023-12-24 19:19:45 -0300

    updating cart quantity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,10 @@
+import { selectProductsCount } from "@/redux/cart/cart.selectors";
+import { useSelector } from "react-redux";
+
 /* eslint-disable @next/next/no-img-element */
 const Header = () => {
+  const productsCount = useSelector(selectProductsCount);
+
   return (
     <header className="bg-slate-950 py-5">
       <div className="header-content container mx-auto flex justify-between">
@@ -7,12 +12,13 @@ const Header = () => {
           className="text-white text-3xl"
         >Redux Store</h1>
 
-        <div className="cart">
+        <div className="cart flex items-center">
           <img
             className="cursor-pointer w-8 h-8"
             src="/cart-icon.png"
             alt="Ícone de sacola de compras"
           />
+          <span className="text-white">({productsCount})</span>
         </div>
       </div>
     </header>
