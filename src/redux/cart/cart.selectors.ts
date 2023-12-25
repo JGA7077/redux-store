08161f823dd0c8d888a575afddb5bc2e0086054b
João Gabriel <joaogabriel-al@hotmail.com>
2023-12-25 18:34:38 -0300

    creating total selector

--- src/redux/cart/cart.selectors.ts
+++ src/redux/cart/cart.selectors.ts
@@ -5,3 +5,9 @@ export const selectProductsCount = (rootReducer: RootReducer) => {
     return acc += curr.quantity
   }, 0)
 }
+
+export const selectProductsTotalPrice = (rootReducer: RootReducer) => {
+  return rootReducer.cartReducer.products.reduce((acc, curr) => {
+    return acc += (curr.price * curr.quantity)
+  }, 0)
+}
