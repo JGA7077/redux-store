03e2a5143c668866ad19cb9fcd5fe61bbfca15a7
João Gabriel <joaogabriel-al@hotmail.com>
2023-12-27 21:03:15 -0300

    product alert

---
+++ src/components/AddedProductAlert/index.tsx
@@ -0,0 +1,16 @@
+interface AddedProductAlert {
+  showAddedProductAlert: boolean;
+}
+
+const AddedProductAlert = ({showAddedProductAlert}: AddedProductAlert) => {
+  return (
+    
+    <figure
+      className={`transition-all duration-700 ease-in py-1 px-5 bg-green-300 text-green-800 rounded-lg border border-green-800 fixed top-24 ${showAddedProductAlert ? 'left-3' : 'left-[-1000%]'}`}
+    >
+      <figcaption>Product Added!</figcaption>
+    </figure>
+  )
+}
+
+export default AddedProductAlert;
